4b42b3fe35d00e87f890f84bd1539dc445b0d13f
Klorridaa <fachriahsani89@gmail.com>
2026-09-09 10:47:50 +0700

    Perbaiki struktur Pertemuan 1

---
+++ Pertemuan 1/latihan-variable-2.py
@@ -0,0 +1,12 @@
+print('== Program Perkenalan ==\n')
+
+nama = input('Fachri Ahsani')
+nim = input('260901011')
+prodi = input('Rekayasa Keamanan Siber')
+print()
+
+print('====================================================')
+print(f' Nama   : {nama}') 
+print(f' NIM    : {nim}') 
+print(f' Prodi  : {prodi}')
+print('====================================================')
---
+++ Pertemuan 1/latihan-variable.py
@@ -0,0 +1,12 @@
+print('== Program Perkenalan ==\n')
+
+nama = input('Silahkan input nama anda: ')
+nim = int(input('Silahkan input NIM anda: '))
+prodi = input('Silahkan input prodi anda: ')
+print()
+
+print('====================================================')
+print(f' Nama   : {nama}') 
+print(f' NIM    : {nim}') 
+print(f' Prodi  : {prodi}')
+print('====================================================')
